97ae7770ea61da622b151a04e49d719e7722f88a
Saucetray <justinsostreofficial@gmail.com>
2018-12-29 14:40:14 -0500

    Just taken from online. Will be changed later.

--- src/server/chess_server.c
+++ src/server/chess_server.c
@@ -1,6 +1,72 @@
-#include <stdlib.h>
-#include <stdio.h>
+// Server side C/C++ program to demonstrate Socket programming 
+#include <unistd.h> 
+#include <stdio.h> 
+#include <sys/socket.h> 
+#include <stdlib.h> 
+#include <netinet/in.h> 
+#include <string.h> 
+#include <arpa/inet.h>
 
-int main() {
-    printf("Hi.");
-}
+#define PORT 12000
+
+int main(int argc, char const *argv[]) 
+{ 
+	int server_fd, new_socket, valread; 
+	struct sockaddr_in address; 
+	int opt = 1; 
+	int addrlen = sizeof(address); 
+	char buffer[1024] = {0}; 
+	char *hello = "Hello from server"; 
+	
+	// Creating socket file descriptor 
+	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) 
+	{ 
+		perror("socket failed"); 
+		exit(EXIT_FAILURE); 
+	} 
+	
+	// Setting socket options.
+	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) 
+	{ 
+		perror("setsockopt"); 
+		exit(EXIT_FAILURE); 
+	} 
+	address.sin_family = AF_INET; 
+	address.sin_addr.s_addr = INADDR_ANY; // Making this socket available to all available interfaces.
+	address.sin_port = htons(PORT); // Attaching the socket to defined PORT 
+	
+	// Forcefully attaching socket to the PORT 
+	if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0) 
+	{ 
+		perror("bind failed"); 
+		exit(EXIT_FAILURE); 
+	} 
+
+	// Start listening for incoming connections. This would make this socket a passive socket for
+	// accepting the connections. 3 specifies the maximum length of queue for pending connections.
+	// If this queue is full, then the client may receive an error.
+	if (listen(server_fd, 3) < 0) 
+	{ 
+		perror("listen"); 
+		exit(EXIT_FAILURE); 
+	} 
+
+	// Accepting the incomming connection.
+	if ((new_socket = accept(server_fd, (struct sockaddr *)&address, 
+					(socklen_t*)&addrlen))<0) 
+	{ 
+		perror("accept"); 
+		exit(EXIT_FAILURE); 
+	} 
+
+	// Read the specified number of bytes into the buffer from the new connection.
+
+	valread = read( new_socket , buffer, 1024); 
+	printf("%s\n",buffer ); 
+
+	// Sending the buffer to the connected client.
+	send(new_socket , hello , strlen(hello) , 0 ); 
+	printf("Hello message sent\n"); 
+
+	return 0; 
+} 
