22c87079026e6ac14e016631b5861cc44c8f0116
Saucetray <justinsostreofficial@gmail.com>
2019-02-24 22:43:18 -0500

    Server side is coming along.

--- src/server/chess_server.c
+++ src/server/chess_server.c
@@ -6,67 +6,109 @@
 #include <netinet/in.h> 
 #include <string.h> 
 #include <arpa/inet.h>
+#include <pthread.h>
 
 #define PORT 12000
 
-int main(int argc, char const *argv[]) 
-{ 
-	int server_fd, new_socket, valread; 
+pthread_t *threads;
+
+typedef struct {
+    int id;
+    int socket;
+} Socket_Info;
+
+
+void  *handle_player(void *args) {
+    
+    printf("Handling the player.\n");
+    fflush(stdout);
+    sleep(2);
+    int valread;
+    char buffer[1024] = {0};
+    char *hello = "Dog.";
+    Socket_Info *info = (Socket_Info*) args;
+    valread = read(info->socket, buffer, 1024); 
+    printf("%s\n",buffer); 
+    
+    // Sending the buffer to the connected client.
+    send(info->socket , hello , strlen(hello) , 0 ); 
+    printf("Hello message sent\n"); 
+
+    return 0;
+}
+
+
+int main() { 
+	int server_fd, new_socket; 
 	struct sockaddr_in address; 
-	int opt = 1; 
+	int opt = 1;
 	int addrlen = sizeof(address); 
-	char buffer[1024] = {0}; 
-	char *hello = "Hello from server"; 
 	
-	// Creating socket file descriptor 
-	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) 
-	{ 
+        int length = 10;
+        threads = calloc(sizeof(pthread_t), length);
+        // Server is made.
+	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) { 
 		perror("socket failed"); 
 		exit(EXIT_FAILURE); 
 	} 
 	
 	// Setting socket options.
-	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) 
-	{ 
+	if (setsockopt(server_fd, SOL_SOCKET, 
+                    SO_REUSEADDR, &opt, sizeof(opt))) { 
 		perror("setsockopt"); 
 		exit(EXIT_FAILURE); 
 	} 
 	address.sin_family = AF_INET; 
-	address.sin_addr.s_addr = INADDR_ANY; // Making this socket available to all available interfaces.
+	address.sin_addr.s_addr = INADDR_ANY; 
 	address.sin_port = htons(PORT); // Attaching the socket to defined PORT 
 	
 	// Forcefully attaching socket to the PORT 
-	if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0) 
-	{ 
+	if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0) { 
 		perror("bind failed"); 
 		exit(EXIT_FAILURE); 
 	} 
 
-	// Start listening for incoming connections. This would make this socket a passive socket for
-	// accepting the connections. 3 specifies the maximum length of queue for pending connections.
-	// If this queue is full, then the client may receive an error.
-	if (listen(server_fd, 3) < 0) 
-	{ 
+	// Start listening for incoming connections. Will deny if 10 in queue
+	if (listen(server_fd, 10) < 0) { 
 		perror("listen"); 
 		exit(EXIT_FAILURE); 
 	} 
 
-	// Accepting the incomming connection.
-	if ((new_socket = accept(server_fd, (struct sockaddr *)&address, 
-					(socklen_t*)&addrlen))<0) 
-	{ 
-		perror("accept"); 
-		exit(EXIT_FAILURE); 
-	} 
+        printf("Accepting Connections to Chess.\n");
 
-	// Read the specified number of bytes into the buffer from the new connection.
+        while(1) {
+	    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, 
+					(socklen_t*)&addrlen))<0) { 
+                fprintf(stderr, "Failure to accept incoming connection.");
+            } else {
+                printf("Connection found!\n");
+                fflush(stdout);
+                int thread_index = -1;
+                for (int i = 0; i < length; i++) {
+                    if (threads[i] == 0) {
+                        thread_index = i;
+                        break;
+                    } 
+                }
 
-	valread = read( new_socket , buffer, 1024); 
-	printf("%s\n",buffer ); 
+                printf("Conenction is being made into a thread.\n");
+                if (thread_index == -1) {
+                    thread_index = length;
+                    length *= 2;
+                    threads = realloc(threads, sizeof(threads) * length);
+                }
 
-	// Sending the buffer to the connected client.
-	send(new_socket , hello , strlen(hello) , 0 ); 
-	printf("Hello message sent\n"); 
+                Socket_Info *info = NULL;
+                info->id = thread_index;
+                info->socket = new_socket;
+                printf("Dog.\n");
+                fflush(stdout);
+                pthread_create(&threads[thread_index], NULL, 
+                        handle_player, (void*) info);
+                printf("Dog.\n");
+	    }
+
+        }
 
 	return 0; 
 } 
